8796ae6290f6fac62e17af1b0f3a77af8a3d3d5d
Josiah Altschuler <josiah.altschuler@gmail.com>
2016-03-03 16:24:54 -0600

    added code to make terms whittle down

--- whittler.php
+++ whittler.php
@@ -23,14 +23,67 @@ class Whittler_widget extends WP_Widget {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
 
-		$cat_args = array(
-			'orderby'	=> 'name',
-			'title_li'	=> ''
-		);
 
-		echo "<ul>";
-		wp_list_categories( apply_filters( 'widget_categories_args', $cat_args ) );
-		echo "</ul>";
+
+		if ($_GET["category"] != "") {
+			echo "<div id='you-searched-for'>";
+			echo "You searched for: ";
+			
+			if ($_GET["category"] != "") {
+				$categoryParameters = explode(" ", $_GET["category"]);
+				foreach ($categoryParameters as $categoryParameter) {
+					$term = get_term_by('slug', $categoryParameter, 'category');
+					$name = $term->name;
+					echo " <a class='topic-term' href='" . esc_url( home_url( '/' ) ) . "resources/evidence-based-practices/?category=" . $_GET["type"] . "'>" . $name . "</a>";
+				}
+			}
+			echo "<p>Number of results: " . $GLOBALS['wp_query']->found_posts . "</p>";
+			echo "<br /><p><a id='clear-search' href='" . esc_url( home_url( '/' ) ) . "'>Clear Search</a></p>";
+			echo "</div>";
+		} else {
+			echo "<p>Number of results: " . $GLOBALS['wp_query']->found_posts . "</p>";
+		}
+
+
+
+		// Get all the terms that are used in this list of posts and put them in an array.
+		// Need to do this so that we don't list category terms used by other post types.
+		$dropdownTopics = array(); //This will collect all the topic terms to place in the sidebar dropdown
+
+		query_posts( 'posts_per_page=-1' );
+		while ( have_posts() ) : the_post();
+			$id = get_the_id();
+			$topicTerms = wp_get_object_terms($id, 'category');
+			if ( ! empty( $topicTerms ) && ! is_wp_error( $topicTerms ) ) {
+				foreach ( $topicTerms as $term ) {
+					if (!in_array($term, $dropdownTopics)) {
+						array_push($dropdownTopics, $term);
+					}
+				}
+			}
+		endwhile;
+		wp_reset_postdata();
+
+		if ( ! empty( $dropdownTopics ) && ! is_wp_error( $dropdownTopics ) ) {
+			foreach ( $dropdownTopics as $term ) {
+				$categorySeparatedTerms = '';
+				$alreadySelected = FALSE;
+
+				if ($_GET["category"] != '') {
+					if (in_array($term->slug, $categoryParameters)) {
+						$alreadySelected = TRUE;
+					}
+					$categorySeparatedTerms = implode("+", $categoryParameters);
+					$categorySeparatedTerms .= "+";
+				}
+
+				if ($alreadySelected == TRUE) {
+					echo '<p>&#x2713; ' . $term->name . '</p>';
+				} else {
+					echo '<a href="' . home_url( '/?category=' ) . $categorySeparatedTerms . $term->slug . '">' . $term->name . '</a><br />';
+				}
+			}
+		}
 
 		echo $args['after_widget'];
 	}
