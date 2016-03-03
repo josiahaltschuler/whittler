3cc4ee0d5ec9693e1a0a8ddb3c0fb2ce88c3051a
Josiah Altschuler <josiah.altschuler@gmail.com>
2016-03-03 10:47:10 -0600

    added main plugin file

---
+++ whittler.php
@@ -0,0 +1,9 @@
+/**
+ * Plugin Name: Whittler
+ * Plugin URI: http://awcdev.com
+ * Description: This plugin creates a category list widget that whittles down the posts on the fly as you select terms.
+ * Version: 1.0.0
+ * Author: Josiah Altschuler
+ * Author URI: http://awcdev.com
+ * License: GPL2
+ */
